1d21c1217d8edada61d4d4a0ed0c2c5742d4a3e5
Gyfu <hthor35@gmail.com>
2018-10-02 19:54:15 +0000

    init

---
+++ verkefni2/forrit.py
@@ -0,0 +1,14 @@
+#Höfundur: Huginn Þór Jóhannsson
+from bottle import *
+@error(404)
+def error404(error):
+    return "404 Not found"
+#A liður
+@route('/')
+def index():
+    return "Main index page"
+
+#B liður
+
+run(host="localhost",port=8080,debug=True)
+#run(host="0.0.0.0",port=argv[1],debug=False)
