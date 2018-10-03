7c7110a4a2d9137eee9e8e7c2a72b5a6f9b14459
Gyfu <hthor35@gmail.com>
2018-10-03 19:03:33 +0000

    verk2 lokið

--- forrit.py
+++ forrit.py
@@ -3,14 +3,23 @@ from bottle import *
 from sys import argv
 @error(404)
 def error404(error):
-    return "404 Not found"
-#A liður
-@route('/index')
-def index():
-    return static_file("index.html",root="static/")
-@route('/myndir/<mynd>')
-def mynd(mynd):
-    return mynd
+    return "404 Not found :D"
 
-#run(host="localhost",port=8080,debug=True)
+@route('/index')
+@route('/')
+def index():
+    return static_file('index.html', root='')
+
+#A liður
+@route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='./static')
+
+@route('/myndir')
+def myndir():
+    mynd = request.query.mynd
+    return template("<img src='{{ mynd }}'>",mynd=mynd)
+    
+
+#run(host="localhost",port=8080,debug=True,reloader=True)
 run(host="0.0.0.0",port=argv[1],debug=False)
